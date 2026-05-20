bbcbacd536af50e6aa702ff9d698b4b760bb9b5f
scmlewis <scmlewis@gmail.com>
2026-05-20 19:03:28 +0800

    add github pages deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/modern-weather-forecast-lab/',
   plugins: [react()],
   optimizeDeps: {
     include: [],
